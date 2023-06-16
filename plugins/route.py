2ef598b922ccb83d7830be5c1dd9a9de919dea0e
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2023-06-16 19:23:45 +0530

    route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,8 +1,7 @@
-#rymme
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CRYSTAL TELEGRAM | https://t.me/Crystal_Anime_Index")
+    return web.json_response("CRYSTAL GAMES | https://t.me/Crystal_Games")
