27e26cfb4fb7ab036acd7c5efc6d93860bc75d00
CRYSTAL HOSTING <crystal.hq.hosting@gmail.com>
2023-06-16 19:34:54 +0530

    config.py

--- config.py
+++ config.py
@@ -1,44 +1,28 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-#Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
 
-#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
-#Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
-#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
-#Port
 PORT = os.environ.get("PORT", "8080")
 
-#Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
 
 DB_NAME = os.environ.get("DATABASE_NAME", "")
 
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001216650755"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋\n\n• Join Our Channel To Get Access To Files Through Links !\n\n🔰 @Crystal_Anime_Index\n🗨️ @Crystal_Anime_Discussion</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {mention} 👋\n\n• Join Our Channel To Get Access To Files Through Links !\n\n🔰: @Crystal_Games\n🗨️: https://t.me/+xLspIoegS145NzU1</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,28 +30,24 @@ try:
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>🗣️ Kageyama:\nHello {mention} 👋\nYou Need To Join Our Channel, So You Can Access My Links !\n\n🔰 @Crystal_Anime_Index\n🗨️ @Crystal_Anime_Discussion</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>🗣️ Kratos:\nHello {mention} 👋\nYou Need To Join Our Channel, So You Can Access My Links !\n\n🔰: @Crystal_Games\n🗨️: https://t.me/+xLspIoegS145NzU1</b>")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-#set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
     DISABLE_CHANNEL_BUTTON = True
 else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "🤖 <b>BOT UPTIME</b> 📊\n\n ⏳: {uptime}"
-USER_REPLY_TEXT = "🔔 Telegram Notification:\n\nKageyama Is Busy With Body Improvement Club Activities, So He Can't Talk To You !\n\nYou Can Type /start For Help !\n\n🔰 @Crystal_Anime_Index\n🗨️ @Crystal_Anime_Discussion\n\n👀: bit.ly/3oE1k0W"
+USER_REPLY_TEXT = "🔔 <b>Telegram Notification</b>:\n\nKageyama Is Busy With Body Improvement Club Activities, So He Can't Talk To You !\n\nYou Can Type /start For Help !\n\n🔰 @Crystal_Anime_Index\n🗨️ @Crystal_Anime_Discussion\n\n👀: bit.ly/3oE1k0W"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
 
-LOG_FILE_NAME = "Kageyamadiary.txt"
+LOG_FILE_NAME = "KratosDiary.txt"
 
 logging.basicConfig(
     level=logging.INFO,
